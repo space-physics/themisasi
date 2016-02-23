e4ee19b7513edac1ab8aa04c3b77add540e07ad6
scivision <scivision@users.noreply.github.com>
2016-02-23 13:07:47 -0500

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(name='themisasi',
 
 #%%
 try:
-    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.run(['conda','install','--yes','--quiet','--file','requirements.txt'])
 except Exception as e:
     print('you will need to install packages in requirements.txt  {}'.format(e))
+    with open('requirements.txt','r') a f:
+        print(f.read())
 
