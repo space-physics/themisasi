d7ee5ed519018a4db9985201ee85b3e4229e2717
scivision <scivision@users.noreply.github.com>
2016-07-11 20:01:18 -0400

    prereq, simplify

--- setup.py
+++ setup.py
@@ -3,20 +3,17 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     pass
-
-with open('README.rst','r') as f:
-	long_description = f.read()
 
 setup(name='themisasi',
       packages=['themisasi'],
 	  description='Utilities for working with THEMIS GBO ASI camera data',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/themisasi',
-	  install_requires=['histutils','pymap3d','astrometry_azel'],
+	  install_requires=['pathlib2',
+                        'histutils','pymap3d','astrometry_azel'],
       dependency_links = [
         'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
         'https://github.com/scienceopen/pymap3d/tarball/master#egg=pymap3d',
