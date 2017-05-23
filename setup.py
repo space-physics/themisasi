c7bf60bb4410f3ec7c1b9bcec26f639948d4d79f
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-23 14:34:37 -0400

    template

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+req = ['nose','numpy','scipy','h5py','matplotlib','networkx','netcdf4']
+pipreq = ['histutils','pymap3d','astrometry_azel','sciencedates',
+          'spacepy']
+          
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:
+    pip.main(['install'] + req)
+pip.main(['install'] + pipreq)
+# %%
 from setuptools import setup
 
-req = ['histutils','pymap3d','astrometry_azel','sciencedates',
-       'nose','numpy','scipy','h5py','matplotlib','networkx','spacepy','netcdf4']
 
 setup(name='themisasi',
+      packages=['themisasi'],
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/themisasi',
       description='reads and plots THEMIS ASI video data of aurora.',
@@ -15,7 +26,5 @@ setup(name='themisasi',
       'Topic :: Scientific/Engineering :: Atmospheric Science',
       'Programming Language :: Python :: 3.6',
       ],
-      packages=['themisasi'],
-	  install_requires=req,
 	  )
 
